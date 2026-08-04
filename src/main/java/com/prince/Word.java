4af51e165f9904bba7645d2a943c72e6fdea2c80
Prince <nkosana.connect@gmail.com>
2026-08-04 22:31:39 +0200

    Feat: Implement Word.java

--- src/main/java/com/prince/Word.java
+++ src/main/java/com/prince/Word.java
@@ -2,4 +2,25 @@ package com.prince;
 
 public class Word {
 
+    private String word;
+    private String meaning;
+    private String usecase;
+
+    public Word(String word, String meaning, String usecase){
+        
+        this.word = word;
+        this.usecase = usecase;
+        this.meaning = meaning;
+
+    }
+
+    public String getWord(){
+        return word;
+    }
+    public String getMeaning() {
+        return meaning;
+    }
+    public String getUsecase() {
+        return usecase;
+    }
 }
