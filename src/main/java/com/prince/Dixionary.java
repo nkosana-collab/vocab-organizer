bc7ae2e27c77bfbc98689d2e65669c7bd822f9aa
Prince <nkosana.connect@gmail.com>
2026-08-05 22:07:41 +0200

    Feat: Implement addWord()

--- src/main/java/com/prince/Dixionary.java
+++ src/main/java/com/prince/Dixionary.java
@@ -2,18 +2,29 @@ package com.prince;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Dixionary {
 
      private static List<Word> words;
+     static Scanner scanner;
 
      public Dixionary(){
 
         words = new ArrayList<>(); // This should be the referance to mySQL lite database.
      }
 
-     public void addWord(String word){
+     public void addWord(String wordString){
         // This should add to mySQL lite database.
+        scanner = new Scanner(System.in);
+
+        System.out.println("DEFINATION: ");
+        String defination = scanner.nextLine();
+        System.out.println("USECASE: ");
+        String usecase = scanner.nextLine();
+
+        Word word = new Word(wordString, defination, usecase);
+        words.add(word);
      }
 
      public void printWord(Word word){
